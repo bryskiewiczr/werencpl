80ec3d0c298dc04dc613f30452fab9b3478b7ec9
rbrysk <bryskiewiczr@protonmail.com>
2024-07-05 23:12:36 +0200

    fix margins

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -14,10 +14,10 @@ const ProductsPage = () => {
               <div className="flex flex-row mb-4">
                 <h1 className="text-2xl md:text-3xl font-black text-left md:text-center">Produkty WERENC</h1>
               </div>
-              <div className="flex flex-row mt-12">
+              <div className="flex flex-row mt-12 space-x-10">
                 <div className="flex flex-col w-1/2">
                   <Link to={"/products/propellerSeries"}><button>
-                    <div className="bg-white hover:scale-110 mx-5 my-5 transition-transform duration-300 shadow-zinc-800 drop-shadow hover:drop-shadow-2xl rounded-lg p-6">
+                    <div className="bg-white hover:scale-110 transition-transform duration-300 shadow-zinc-800 drop-shadow hover:drop-shadow-2xl rounded-lg p-6">
                       <img src={CS3Propeller} alt="Carbonowe śmigło WERENC CS3"/>
                       <h1 className="text-3xl">Śmigła WERENC</h1>
                     </div>
@@ -26,7 +26,7 @@ const ProductsPage = () => {
                 <div className="flex flex-col w-1/2">
                   <button>
                       <div
-                        className="bg-white hover:scale-110 mx-5 my-5 transition-transform duration-300 shadow-zinc-800 drop-shadow hover:drop-shadow-2xl rounded-lg p-6">
+                        className="bg-white hover:scale-110 transition-transform duration-300 shadow-zinc-800 drop-shadow hover:drop-shadow-2xl rounded-lg p-6">
                       <img src={Throttle} alt="Carbonowe śmigło WERENC CS3"/>
                       <h1 className="text-3xl">Manetki WERENC</h1>
                     </div>
